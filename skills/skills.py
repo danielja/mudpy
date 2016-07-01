0828b9793abca6833d1af49ce9e77d8cf87b3ad1
Danny <none>
2016-07-01 22:57:36 +0000

    pull and parse all skills that you have access to

--- skills/skills.py
+++ skills/skills.py
@@ -1,11 +1,13 @@
 import meta
 import sage
-from sage import player, triggers, aliases
+from sage import player, triggers, aliases, gmcp
 from sage.signals import pre_shutdown
-from sage.signals.gmcp import skills
+from sage.signals.gmcp import skills, skill_info
 import time
 import MySQLdb as mysql
 import MySQLdb.cursors
+
+SKILL_UNKNOWN = '*** You have not yet learned this ability ***'
 
 class SkillMap(object):
 
@@ -24,8 +26,56 @@ class SkillMap(object):
             self.login = [x.strip().split(':') for x in f.readlines()][0]
         self.load()
         
+    def show_skill_info(self, group, skill, info, **kwargs):
+        info_lines = info.splitlines()
+        info_map = {'known':True}
+        field = "default"
+
+        # Skills are received in order - keep track of that order
+        if group not in self.skills:
+            self.skills[group] = []
+        if skill not in self.skills[group]:
+            self.skills[group].append(skill)
+        info_map['idx'] = self.skills[group].index(skill)+1
+        for line in info_lines:
+            if line == SKILL_UNKNOWN:
+                info_map['known'] = False
+            elif line.endswith(":"):
+                field = line.strip(':').lower()
+            elif len(line) > 0 and field in info_map:
+                info_map[field] += ";%s"%line.strip()
+            elif len(line) > 0:
+                info_map[field] = line.strip()
+            elif field not in info_map:
+                info_map[field] = ''
+        for field in ['works on/against', 'details','cooldown']:
+            if field not in  info_map:
+                info_map[field] = ''
+
+        db = mysql.connect(host=self.login[0], user=self.login[1],passwd=self.login[2],
+                db='achaea',cursorclass=MySQLdb.cursors.DictCursor)
+        cur=db.cursor()
+
+        info_map['workson'] = info_map['works on/against']
+
+        for key,value in info_map.iteritems():
+            if key in ['syntax','details','workson','resource','drains','cooldown','idx']:
+                cur.execute('INSERT into achaea.skill_info'
+                    ' (skill,ability,' +key+ ') '
+                    ' VALUES '
+                    ' (%s, %s, %s) '
+                    ' ON DUPLICATE KEY UPDATE skill=skill, ability=ability, ' +key+ '=values(' +key+ ')'
+                    ';',(group, skill,value))
+        cur.close()
+        db.commit()
+        db.close()
+
+
+
+
+
     def skills_update(self, **kwargs):
-        #print self.skills
+        print self.skills
         for skill, ablist in kwargs['skills'].iteritems():
             if skill not in self.skills:
                 print skill
@@ -111,6 +161,9 @@ smap = SkillMap()
 def shutdown(**kwargs):
     smap.save()
 
+
+
+skill_info.connect(smap.show_skill_info)
 skills.connect(smap.skills_update)
 pre_shutdown.connect(shutdown)
 
@@ -118,6 +171,10 @@ pre_shutdown.connect(shutdown)
 skill_triggers = triggers.create_group('skill', app='skills')
 skill_aliases  = aliases.create_group('skill', app='skills')
 track_shortnames = False
+
+@skill_aliases.exact(pattern="skillsup", intercept=True)
+def updateskills(alias):
+    gmcp.get_skills()
 
 @skill_aliases.exact(pattern="skills load", intercept=True)
 def loadstuff(alias):
